ae73e0b01d132efe76f757cb59b44323eb9baafc
SyntaxTechnologies <instructor@syntaxtechs.com>
2023-02-23 20:11:28 -0500

    Multiple inheritance

--- src/com/syntax/review9/Drawable.java
+++ src/com/syntax/review9/Drawable.java
@@ -1,31 +1,55 @@
 package com.syntax.review9;
 
+//collection of public static final variables and public abstract methods
+
 public interface Drawable {
-    public static final String TOOL="Pencil";
+    String TOOL = "Pencil"; //by default it is public static final variable
+
     void draw(); // by default this method is public abstract
 
     //above is how Interface used to be before Java 8
     //after Java 8 static and default methods were added to the Interface
-
-    default void print(){
-        System.out.println("We are drawing object using "+TOOL);
+    default void print() {
+        System.out.println("We are drawing object using " + TOOL);
     }
 
-    static void erase(){
+    static void erase() {
         System.out.println("All drawings are erasable");
     }
 }
 
-class Rectangle implements Drawable {
+abstract class Shape {
 
-    public void draw(){
+    public String color;
+    protected double width, length;
+
+    Shape(String color, double length, double width) {
+        this.color = color;
+        this.length = length;
+        this.width = width;
+    }
+
+    public abstract double calculateArea();
+}
+
+class Rectangle extends Shape implements Drawable {
+
+    Rectangle(String color, double length, double width){
+        super(color, length, width);
+    }
+
+    public double calculateArea(){
+
+        return length*width;
+    }
+    public void draw() {
         System.out.println("I am drawing rectangle");
     }
 }
 
 class Square implements Drawable {
 
-    public void draw(){
+    public void draw() {
         System.out.println("I am drawing Square");
     }
 
